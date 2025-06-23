228b03a19efd77397c160feed50584e07c48cad8
ArendBR <Arend.Rijpkema@gmail.com>
2025-06-23 02:43:21 +0200

    hthhh

--- applicatie/menu.php
+++ applicatie/menu.php
@@ -2,8 +2,10 @@
  require_once "header.php";
  //require_once "functions/createmenu.php";
  require_once "sidebar.php";
- require_once "functions/createwinkelmand.php"; 
- require_once "functions/toevoegenwinkelmand.php";
+ require_once "db_connectie.php";
+ //require_once "functions/createwinkelmand.php"; 
+ //require_once "functions/toevoegenwinkelmand.php";
+ require_once "functions/zoekfunctie.php";
 ?>
 
 <!DOCTYPE html>
@@ -21,17 +23,40 @@
 
     <?php
     createHeader();
+    createSidebar();
     ?>
-    <?php
-     createSidebar();
-    ?>
- 
+  <?php  
+  //if   
+  $db = maakVerbinding(); 
+    $sql = "SELECT name, type_id, Pizza_Order_Product.quantity, price
+            FROM Product
+            INNER JOIN Pizza_Order_Product ON Product.name = Pizza_Order_Product.product_name;";
+//$sql = "SELECT * FROM Product, User1, Pizza_Order_Product;";
+   // $query = $db->prepare($sql);
+   $query = $db -> query($sql);
+   // $results = $query->fetchAll(PDO::FETCH_ASSOC);
+   $results = $query->fetchAll(PDO::FETCH_ASSOC);
+   vardump($results);
+   //if (isset($results)) {
+   //  echo "$results[]";
+  //}  else{
+    
+    echo "geen resultaat";
+   // }
+    //$query -> execute();
+   //echo $query["price"]; 
+  // while ($results = $query->fetchAll(PDO::FETCH_ASSOC)) {
+   // echo $results["price"];
+  // echo $results["name"], $results["type_id"], $results["quantity"], $results["price"] ;
+  //  foreach ($results as $result) {
+ ?>
     <div class="content-right">
       <h1 class="menu">Menu.</h1>
-       <form action="functions/toevoegenwinkelmand.php" method="POST"> 
+       <form action="" method="POST"> 
         <label for="zoek">zoeken:</label><br/>
         <input type="text" placeholder="menu" id="zoek" name="zoek"/>
         <input type="submit"/><br/>
+        <?php zoekFunctie(); ?>
  <a href="winkelmand.php"><img src="winkelmand.jpg" alt="winkelmand" width="50" height="60"></a>
         <div class=outer-wrapper>
           <div class=table-wrapper>
@@ -42,29 +67,24 @@
               <th>Prijs</th>
               <th>Toevoegen aan winkelmand</th>
 
-             
-              <?php
-              createmenuoverzicht();
-              ?>
-          <!--     <div class="producttype">
-           <td> <?php// $results["type_id"] ?>  </td> 
-             <td> <?php //$_SESSION["type_id"] ?>  </td> 
-              </div> -->
-              <!-- <input type='hidden' name='name' value='<?php// require_once "functions/toevoegenwinkelmand.php"; $result["name"];  ?>'> -->
-
-                <td> <input type='hidden' name='name' value='<?php $product ?>'> </td> 
-               <input type='hidden' name='type_id' value='<?php $result["type_id"] ?>'>
-               <input type='hidden' name='price' value='<?php $price ?>'>
-
-              <!-- <input type='hidden' name='name' value='<?php //$result["name"]; ?>'>
-               <input type='hidden' name='type_id' value='<?php //$result["type_id"]; ?>'>
-               <input type='hidden' name='price' value='<?php //$result["price"]; ?>'> -->
+              <tr>  
+                <td> <?php echo $results['price']; ?> </td>
+              <td> <input type='text' name='product' value='<?php $results["name"] ?>'> </td>
+                <td> <input type='text' name='producttype' value='<?php $results["type_id"] ?>'> </td>
+                <td> <input type='number' min='1'  name='quantity' value=1> </td>
+                <td> <input type='text' name='price' value='<?php $results["price"] ?>'> </td>
+                <td> <input type='submit' name='toevoegen' value=toevoegen> </td>
+              </tr>
+            
             </table>
           </div>
         </div>
       </form>
     </div>
-
+<?php 
+  //  }
+  //}
+   ?>
   </main>
 
 <?php
@@ -73,3 +93,22 @@
 </body>
 
 </html>
+
+   <?php
+
+              // createmenuoverzicht();
+              ?>
+          <!--     <div class="producttype">
+           <td> <?php// $results["type_id"] ?>  </td> 
+             <td> <?php //$_SESSION["type_id"] ?>  </td> 
+              </div> -->
+              <!-- <input type='hidden' name='name' value='<?php// require_once "functions/toevoegenwinkelmand.php"; $result["name"];  ?>'> -->
+
+ <!--   <td> <input type='hidden' name='name' value='<?php //$product ?>'> </td> 
+               <input type='hidden' name='type_id' value='<?php //$result["type_id"] ?>'>
+               <input type='hidden' name='price' value='<?php //$price ?>'> -->
+
+              <!-- <input type='hidden' name='name' value='<?php //$result["name"]; ?>'>
+               <input type='hidden' name='type_id' value='<?php //$result["type_id"]; ?>'>
+               <input type='hidden' name='price' value='<?php //$result["price"]; ?>'> -->
+
